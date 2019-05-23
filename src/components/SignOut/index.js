b8e00bbf663315eaf13ea09338f8236b7771554d
Richard-NG <richard.chihong.ng@gmail.com>
2019-05-23 12:35:46 +0800

    added SignOut button

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { withFirebase } from "../Firebase";
 
-export default function Page(props) {
-  return <div>SignOut</div>;
-}
+const SignOutButton = ({ firebase }) => (
+  <button type="button" onClick={firebase.doSignOut}>
+    Sign Out
+  </button>
+);
+
+export default withFirebase(SignOutButton);
