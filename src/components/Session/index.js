f178d2eb0a1a0fe24f98139b5560d18e5cfe0cb9
Richard-NG <richard.chihong.ng@gmail.com>
2019-05-23 16:12:53 +0800

    added withAuthorization

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,6 +1,7 @@
 import AuthUserContext from "./context";
 import withAuthUserProvider from "./withAuthUserProvider";
+import withAuthorization from "./withAuthorization";
 
 // just for encapsulation
 
-export { AuthUserContext, withAuthUserProvider };
+export { AuthUserContext, withAuthUserProvider, withAuthorization };
