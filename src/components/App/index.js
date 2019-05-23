53fe2ed82ff507221772ec7850019d6ea259df6d
Richard-NG <richard.chihong.ng@gmail.com>
2019-05-23 11:51:33 +0800

    add dummy pages to allow app start

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "../Navigation";
 import LandingPage from "../Landing";
 import SignUpPage from "../SignUp";
@@ -9,6 +9,7 @@ import HomePage from "../Home";
 import AccountPage from "../Account";
 import AdminPage from "../Admin";
 import * as ROUTES from "../../constants/routes";
+
 const App = () => (
   <Router>
     <div>
@@ -24,4 +25,5 @@ const App = () => (
     </div>
   </Router>
 );
+
 export default App;
