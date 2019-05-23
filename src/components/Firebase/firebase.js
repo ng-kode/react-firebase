753bcd1bdc5a7d2628ac7846369ac67bb58cabfc
Richard-NG <richard.chihong.ng@gmail.com>
2019-05-23 10:54:03 +0800

    created react project, setup firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,20 @@
+import app from "firebase/app";
+
+// Remember NOT to add quotes around the values on the .env file
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
