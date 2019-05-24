c1e5b622605b02ef117581a2b3ac360889050403
Richard-NG <richard.chihong.ng@gmail.com>
2019-05-24 18:51:52 +0800

    implement UserList UserItem

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import { compose } from "recompose";
 import { withFirebase } from "../Firebase";
 import { withAuthorization, withEmailVerification } from "../Session";
 import * as ROLES from "../../constants/roles";
+import * as ROUTES from "../../constants/routes";
 
-class AdminPage extends Component {
+class UserListBase extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -36,39 +38,105 @@ class AdminPage extends Component {
     const { users, loading } = this.state;
 
     return (
-      <div>
-        <h1>Admin</h1>
-
+      <Fragment>
         {loading && <div>Loading...</div>}
 
-        <UserList users={users} />
+        {users.length && (
+          <ul>
+            {users.map(user => (
+              <li key={user.uid}>
+                <span>
+                  <strong>ID: </strong> {user.uid}
+                </span>
+                <span>
+                  <strong>Email</strong> {user.email}
+                </span>
+                <span>
+                  <strong>Username</strong> {user.username}
+                </span>
+                <span>
+                  <Link
+                    to={{
+                      pathname: `${ROUTES.ADMIN}/users/${user.uid}`,
+                      user
+                    }}
+                  >
+                    Details
+                  </Link>
+                </span>
+              </li>
+            ))}
+          </ul>
+        )}
+      </Fragment>
+    );
+  }
+}
+
+const UserList = withFirebase(UserListBase);
+
+class UserItemBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      user: { ...props.location.user } || null
+    };
+  }
+
+  componentDidMount() {
+    if (this.state.user) {
+      return;
+    }
+
+    this.setState({ loading: true });
+    this.props.firebase
+      .user(this.props.match.params.id)
+      .on("value", snapshot => {
+        this.setState({
+          loading: false,
+          user: snapshot.val()
+        });
+      });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.user(this.props.match.params.id).off();
+  }
+
+  render() {
+    const { user, loading } = this.state;
+
+    return (
+      <div>
+        {loading && <p>Loading...</p>}
+        {user &&
+          Object.keys(this.state.user).map(key => (
+            <div key={key}>
+              <strong>{key}: </strong> {JSON.stringify(user[key])}
+            </div>
+          ))}
       </div>
     );
   }
 }
 
-const UserList = ({ users }) => (
-  <ul>
-    {users.map(user => (
-      <li key={user.uid}>
-        <span>
-          <strong>ID: </strong> {user.uid}
-        </span>
-        <span>
-          <strong>Email</strong> {user.email}
-        </span>
-        <span>
-          <strong>Username</strong> {user.username}
-        </span>
-      </li>
-    ))}
-  </ul>
+const UserItem = withFirebase(UserItemBase);
+
+const AdminPage = () => (
+  <div>
+    <h1>Admin</h1>
+
+    <Switch>
+      <Route exact path={ROUTES.ADMIN_USER_DETAIL} component={UserItem} />
+      <Route exact path={ROUTES.ADMIN} component={UserList} />
+    </Switch>
+  </div>
 );
 
 const condition = user => user && user.roles[ROLES.ADMIN];
 
 export default compose(
   withAuthorization(condition),
-  withFirebase,
   withEmailVerification
 )(AdminPage);
